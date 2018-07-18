efe04a9e2df4357af0f44855eb6f5ca25f7fa337
Eli Stavitski <elistavitski@gmail.com>
2018-07-18 18:01:03 -0400

    Added scheduling and emailing to users

---
+++ startup/35-create_draft.py
@@ -0,0 +1,37 @@
+
+
+
+
+'''
+_______________
+NOTE ON SAFETY APPROVAL FORM – PLEASE READ:
+ 
+Recent safety issues at various beamlines at NSLS-II and other facilities revealed that the safety approval forms (SAF), an essential tool for work planning, do not always contain all information, necessary to evaluate the hazards associated with the experiments. Be advised that the facility will enforce timely submission, a complete description of the experiment including a complete list of all samples, chemicals, and equipment used during the preparation and the experiment itself. Failure to submit a complete SAF in a timely fashion may result in loss of beamtime. 
+_______________
+NOTE ON ELECTRICAL EQUIPMENT – PLEASE READ:
+ 
+If you plan to bring electrical equipment for your experiment, please contact Jerome Malley, Electrical Equipment Inspection (EEI) Manager at malley@bnl.gov as soon as practical. Be prepared to provide manuals, photos, electrical diagrams and schematics.  Please include beamline staff in your communication as some pre-approved electrical equipment (e.g., potentiostat) is available at the beamline as well as NSLS-II research equipment pool. See attached EEI Criteria Guide for further information.
+ 
+Please be advised that the BNL Guest registration should be submitted as soon as you receive this notification. It may take up to several months for your registration to be approved. 
+ Below is the list of activities to be complete prior to your beamtime.
+ 1. You will be contacted by one of the beamline staff before August 31 to discuss specific needs for your experiment. Please be prepared to provide information about required chemicals, compressed gases, and lab needs. Means of chemicals transportation to BNL site and back to home institution should be clarified.
+2. To ensure that all equipment, chemicals and lab infrastructure is available at the beginning of your beamtime, we ask you to submit the Safety Approval Form (SAF) AT LEAST four weeks before the starting date. Your SAF should list ALL chemicals to be used during your experiments (including solvents and reference materials). In addition, any additional equipment not provided by the beamline, has to be identified and described in the SAF. Provide detailed information on sample processing and any safety measures necessary. 
+3. Please complete all required online user training at least one week before the starting date.  
+Please consult NSLS-II user guide (https://www.bnl.gov/ps/userguide/) or contact ISS beamline for more information.
+ 
+If your experiment starts at 8am, we expect your user group to arrive no later than 1pm on the day prior to the first day of the beamtime to set up and perform in-person training. This assures smooth transition between the user groups.
+ 
+With kind regards,
+ISS beamline staff.'
+
+'''
+
+
+
+
+
+
+
+
+
+
---
+++ startup/99-summary.py
@@ -0,0 +1,20 @@
+def push_schedule(filename=None, no_email=False, no_cal=False):
+    if filename is None:
+        filename = '/Users/elistavitski/Documents/Running Projects /ISS operations/2018-2 Cycle/2018 2 ISS schedule list.xlsx'
+    schedule = read_excel(filename)
+    drafts = create_html_drafts(schedule)
+    events = create_events(schedule)
+    if not no_email:
+        for draft in drafts:
+            upload_draft(draft)
+    if not no_cal:
+        for event in events:
+            cal=serviceCalendar.events().insert(calendarId='primary', body=event).execute()
+            print(cal)
+
+
+# In [5]: ev = serviceCalendar.events().insert(calendarId='primary', body=events[0]).execute()
+#
+# In [6]: ev = serviceCalendar.events().insert(calendarId='primary', body=events[1]).execute()
+#
+# In [7]:
