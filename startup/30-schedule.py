a65eaa4f3ca8747141735a96522bbdd5345da75e
Eli Stavitski <elistavitski@gmail.com>
2018-11-02 19:46:34 -0400

    Local directory changes

--- startup/30-schedule.py
+++ startup/30-schedule.py
@@ -65,7 +65,7 @@ def create_drafts(schedule):
 
 def create_html_drafts(schedule):
     drafts = []
-    fid = open('/Users/elistavitski/Documents/Running Projects /ISS operations/letter.html', 'r')
+    fid = open('/Users/elistavitski/Running Projects/ISS operations/letter.html', 'r')
     letter = fid.read().replace('\n', '')
     print(letter)
     print(type(letter))
